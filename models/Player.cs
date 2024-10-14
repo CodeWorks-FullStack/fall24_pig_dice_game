0434351ca0b15ba0d9c9826d6ac2eb0a0d032935
Jeremy Fowler <jembyfowler@gmail.com>
2024-10-14 11:36:48 -0600

    added constructor to player

--- models/Player.cs
+++ models/Player.cs
@@ -2,6 +2,14 @@ namespace pig_dice_game.models;
 
 public class Player
 {
+  // NOTE this is the constructor of the Player class
+  public Player(string name)
+  {
+    // this.Name = name;
+    Name = name;
+    Score = 0;
+  }
+
   public string Name { get; set; }
 
   // NOTE unsigned int is positive only
