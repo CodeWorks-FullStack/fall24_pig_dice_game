f468e96989328c683cb02ef8ed6ee5af1ef9aa7a
Jeremy Fowler <jembyfowler@gmail.com>
2024-10-14 11:49:44 -0600

    Adding players to list

--- Program.cs
+++ Program.cs
@@ -3,9 +3,12 @@
 // i can stop rolling dice at any time to add all rolls to my score
 // if you roll a 1, you can not add to your score and you pass to the next player
 
+// NOTE using is very similar to import, but all public definitions under this namespace are imported instead of single values
+using pig_dice_game.models;
+
 internal class Program
 {
-  static List<string> PlayerNames = [];
+  static List<Player> Players = [];
 
 
   // NOTE entry point to application
@@ -13,26 +16,26 @@ internal class Program
   {
     Console.Clear();
     Console.WriteLine("PIG DICE GAME!!!!!");
-    string playerOneName = AddPlayer();
-    PlayerNames.Add(playerOneName); // .push()
-    string playerTwoName = AddPlayer();
-    PlayerNames.Add(playerTwoName); // .push()
+    Player playerOne = AddPlayer();
+    Players.Add(playerOne); // .push()
+    Player playerTwo = AddPlayer();
+    Players.Add(playerTwo); // .push()
 
     // Count is similar to length
-    for (int i = 0; i < PlayerNames.Count; i++)
+    for (int i = 0; i < Players.Count; i++)
     {
-      string playerName = PlayerNames[i];
-      Console.WriteLine(playerName);
+      Player player = Players[i];
+      Console.WriteLine($"Player {i + 1} is " + player.Name);
     }
   }
 
-  static string AddPlayer()
+  static Player AddPlayer()
   {
-    Console.WriteLine("Enter name for player");
+    Console.WriteLine($"Enter name for player {Players.Count + 1}");
 
     string playerName = Console.ReadLine();
 
-    Console.WriteLine($"Player name is {playerName}");
-    return playerName;
+    Player newPlayer = new Player(playerName);
+    return newPlayer;
   }
 }
